fd617b45ba508bad918d8b5a4e30c455a4ee26d0
Ram Mandal <ram.mandal@8squarei.com>
2026-02-18 09:00:19 +0545

    initial commit for security library

---
+++ src/main/java/com/ronem/rupiasecuritylib/RupiaSecurityLibApplication.java
@@ -0,0 +1,13 @@
+package com.ronem.rupiasecuritylib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupiaSecurityLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RupiaSecurityLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronem/rupiasecuritylib/RupiaSecurityLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronem.rupiasecuritylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupiaSecurityLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
